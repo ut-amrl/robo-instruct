1116595cdd21b4e2beb6dd0e0e7fd69635a7a26e
zichaohu <zichao@utexas.edu>
2024-07-07 17:53:09 -0500

    adapt roboeval

---
+++ roboeval/benchmark/tasks/rejection_sampling/arbitrary_state.py
@@ -0,0 +1,3 @@
+from roboeval.benchmark.simulator import State
+
+arbitrary_state = State().addLocation(".*").addLocation(".*").addLocation(".*").addRobotLocation("start_loc").addAgent(".*", ".*", [r".*"]).addObject(".*", ".*")
